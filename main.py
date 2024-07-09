933e51a005465940dec2f1d39c0e41fb4ce832ad
Dan Schlicht <28845647+djschlicht@users.noreply.github.com>
2024-07-09 19:50:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from force-backup-automator import BackupController
+from botocore.exceptions import NoCredentialsError, ClientError
 import boto3
+import json
 import subprocess
 import datetime
 
-# download chromedriver from web
-
-# use secrets manager to get salesforce credentials
+creds = get_aws_credentials(secret_name, region_name)
 
 backup_instance = BackupController(driver_location='./chromedriver',
                                    org_link='ORG MAIN URL',
                                    is_headless=0)
 
-
-# get the args below from secrets manager
 backup_instance.download_backups(download_location='TARGET LOCATION',
                                  backup_url='ORG URL/lightning/setup/DataManagementExport/home',
                                  user_name='USERNAME',
