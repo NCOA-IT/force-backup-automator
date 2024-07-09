b72a7027c785f832825b2ad37287f995b394d8b5
Dan Schlicht <28845647+djschlicht@users.noreply.github.com>
2024-07-09 19:41:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from force-backup-automator import BackupController
 import boto3
 import subprocess
+import datetime
 
 # download chromedriver from web
 
@@ -10,11 +11,15 @@ backup_instance = BackupController(driver_location='./chromedriver',
                                    org_link='ORG MAIN URL',
                                    is_headless=0)
 
-# modify download_backups to upload completed file to s3 bucket s3://ORGNAME/DATE/
+
+# get the args below from secrets manager
 backup_instance.download_backups(download_location='TARGET LOCATION',
                                  backup_url='ORG URL/lightning/setup/DataManagementExport/home',
                                  user_name='USERNAME',
-                                 password='PASSWORD')
+                                 password='PASSWORD',
+                                 bucket='',
+                                 date=datetime.now(),
+                                 org_name=org_name)
 
 # use subprocess to issue shutdown command
 subprocess.run("shutdown")
