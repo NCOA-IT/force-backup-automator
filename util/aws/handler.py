c7c8a694bb7aa2a24e4a5e5c6f1535933344e430
Dan Schlicht <28845647+djschlicht@users.noreply.github.com>
2024-07-09 19:13:23 -0400

    Update handler.py

--- util/aws/handler.py
+++ util/aws/handler.py
@@ -18,9 +18,9 @@ def handler(event, context):
           cd /home/ec2-user/
           git clone https://github.com/djschlicht/force-backup-automator.git     
           cd force-backup-automator
+          curl -O https://storage.googleapis.com/chrome-for-testing-public/126.0.6478.126/linux64/chrome-linux64.zip
           python3 -m pip install setuptools
           python3 setup.py install
-          python3 util/downloadChromedriver.py
           python3 main.py
           shutdown -h now
           """
